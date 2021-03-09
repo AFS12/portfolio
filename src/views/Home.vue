1df797c58cd214db6f6ec4eaf944711292f3daed
AFS12 <arthurmbayres@gmail.com>
2021-03-09 17:00:33 -0300

    [ayres] - first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    
+    <!-- <v-parallax src="..\assets\background2.jpg"></v-parallax> -->
+     <v-container fluid>
+       
+    </v-container>
+
+    <footer>
+      <h1>a</h1>
+    </footer>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Home',
+}
+</script>
+
+<style>
+#app {
+  background: url('../assets/background.jpg')
+    no-repeat center center fixed !important;
+  background-size: cover;
+}
+</style>

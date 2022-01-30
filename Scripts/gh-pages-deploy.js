4db22c35799db6025330e2b41ec7441a642d1934
AFS12 <arthurmbayres@gmail.com>
2022-01-30 19:39:08 -0300

    Atualização script deploy

--- Scripts/gh-pages-deploy.js
+++ Scripts/gh-pages-deploy.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
-const execa = require("execa");
-const fs = require("fs");
+import {execa} from "execa";
+import fs from "fs";
 (async () => {
   try {
     await execa("git", ["checkout", "--orphan", "gh-pages"]);
@@ -11,9 +11,8 @@ const fs = require("fs");
     const folderName = fs.existsSync("dist") ? "dist" : "build";
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
-    console.log("Pushing to gh-pages...");
+    console.log("Pushing to build...");
     await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
-    await execa("rm", ["-r", folderName]);
     await execa("git", ["checkout", "-f", "master"]);
     await execa("git", ["branch", "-D", "gh-pages"]);
     console.log("Successfully deployed, check your settings");

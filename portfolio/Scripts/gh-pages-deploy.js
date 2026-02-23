3b0f5f84a8b6292038873c3b5325b4e8526ab94d
AFS12 <arthurmbayres@gmail.com>
2026-02-23 02:48:21 -0300

    Refatorar script de implantação para gh-pages, removendo comandos desnecessários e melhorando a legibilidade

--- portfolio/Scripts/gh-pages-deploy.js
+++ portfolio/Scripts/gh-pages-deploy.js
@@ -1,26 +1,58 @@
-/* eslint-disable no-console */
 import { execa } from "execa";
 import fs from "fs";
+import path from "path";
+
+const projectDir = process.cwd();
+const rootDir = path.resolve(projectDir, "..");
+
 (async () => {
   try {
-    await execa("cd ..");
-    await execa("git", ["checkout", "--orphan", "gh-pages"]);
-    // eslint-disable-next-line no-console
     console.log("Building started...");
-    await execa("cd portfolio");
-    await execa("npm", ["run", "build"]);
-    // Understand if it's dist or build folder
-    await execa("cd ..");
-    const folderName = fs.existsSync("dist") ? "dist" : "build";
-    await execa("git", ["--work-tree", folderName, "add", "--all"]);
-    await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
-    console.log("Pushing to build...");
-    await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
-    await execa("git", ["checkout", "-f", "master"]);
-    await execa("git", ["branch", "-D", "gh-pages"]);
-    console.log("Successfully deployed, check your settings");
+    await execa("npm", ["run", "build"], {
+      cwd: projectDir,
+      stdio: "inherit",
+    });
+
+    const folderName = fs.existsSync(
+      path.join(projectDir, "dist")
+    )
+      ? "dist"
+      : "build";
+    await execa("git", ["checkout", "--orphan", "gh-pages"], {
+      cwd: rootDir,
+      stdio: "inherit",
+    });
+
+    await execa(
+      "git",
+      ["--work-tree", path.join(projectDir, folderName), "add", "--all"],
+      { cwd: rootDir, stdio: "inherit" }
+    );
+
+    await execa(
+      "git",
+      ["--work-tree", path.join(projectDir, folderName), "commit", "-m", "gh-pages"],
+      { cwd: rootDir, stdio: "inherit" }
+    );
+
+    await execa(
+      "git",
+      ["push", "origin", "HEAD:gh-pages", "--force"],
+      { cwd: rootDir, stdio: "inherit" }
+    );
+
+    await execa("git", ["checkout", "-f", "master"], {
+      cwd: rootDir,
+      stdio: "inherit",
+    });
+
+    await execa("git", ["branch", "-D", "gh-pages"], {
+      cwd: rootDir,
+      stdio: "inherit",
+    });
+
+    console.log("Successfully deployed 🚀");
   } catch (e) {
-    // eslint-disable-next-line no-console
     console.log(e.message);
     process.exit(1);
   }
